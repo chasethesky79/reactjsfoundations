1427513edba466fade9288acbb32a32179e16844
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-04-03 15:22:25 -0500

    Implemented dynamic form state management

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const [names, setNames] = useState([]);
-  const [name, setName] = useState('');
+  const [formState, setFormState] = useState({
+     firstName: '',
+     lastName: ''
+  });
   const handleSubmit = evt => {
-    setNames([ ...names, name]);
-    setName('')
+    setNames([ ...names, formState]);
+    setFormState({ firstName: '', lastName: ''});
     evt.preventDefault();
   }
+  const handleOnChange = (event, name) => {
+    const { target: { value } } = event;
+    setFormState({ ...formState, [name]: value });
+  }
+  const { firstName, lastName } = formState;
   return (
     <div className='App'>
       <h1>Sign Up Sheet</h1>
       <form onSubmit={handleSubmit}>
-        <input placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+        <input placeholder='First Name' value={firstName} onChange={(e) => handleOnChange(e, 'firstName')}/>
+        <input placeholder='Last Name' value={lastName} onChange={(e) => handleOnChange(e, 'lastName')}/>
         <input type='submit'/>
         <ul>
-          {names.map((name, index) => <li key={index}>{name}</li>)}
+          {names.map((name, index) => <li key={index}>{name.firstName}, {name.lastName}</li>)}
         </ul>
       </form>
     </div>
