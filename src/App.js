91a7f8dfdde646b1e0720c264220312d4fd5abf3
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-04-03 14:36:56 -0500

    Implemented state management with controlled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [names, setNames] = useState([]);
+  const [name, setName] = useState('');
+  const handleSubmit = evt => {
+    setNames([ ...names, name]);
+    setName('')
+    evt.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Sign Up Sheet</h1>
+      <form onSubmit={handleSubmit}>
+        <input placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+        <input type='submit'/>
+        <ul>
+          {names.map((name, index) => <li key={index}>{name}</li>)}
+        </ul>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
