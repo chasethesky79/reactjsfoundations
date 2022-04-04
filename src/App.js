42024662ac580a392ff8367f42267bfb3e7509d9
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-04-03 21:02:45 -0500

    Added field validation to form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const nameDisplayMap = {
+  firstName: 'First Name',
+  lastName: 'Last Name'
+}
+
 function App() {
   const [names, setNames] = useState([]);
   const [formState, setFormState] = useState({
      firstName: '',
-     lastName: ''
+     lastName: '',
+     errors: {
+      firstName: '',
+      lastName: '',
+     }
   });
   const handleSubmit = evt => {
+  const errors = buildErrors(formState);
+  if (Object.keys(errors).length > 0) {
+    setFormState({ ...formState, errors })
+  } else {
     setNames([ ...names, formState]);
     setFormState({ firstName: '', lastName: ''});
-    evt.preventDefault();
   }
+  console.log(`FORM STATE ${JSON.stringify(formState)}`);
+  evt.preventDefault();
+  } 
   const handleOnChange = ({ target: { name, value }}) => {
     setFormState({ ...formState, [name]: value });
   }
-  const { firstName, lastName } = formState;
+  const { firstName, lastName, errors } = formState;
   return (
     <div className='App'>
       <h1>Sign Up Sheet</h1>
       <form onSubmit={handleSubmit}>
-        <input placeholder='First Name' name='firstName' value={firstName} onChange={handleOnChange}/>
-        <input placeholder='Last Name' name='lastName' value={lastName} onChange={handleOnChange}/>
+        <input placeholder='First Name' name='firstName' value={firstName} onChange={handleOnChange}/><span style={{ color: 'red' }}>{ errors?.firstName }</span>
+        <input placeholder='Last Name' name='lastName' value={lastName} onChange={handleOnChange}/><span style={{ color: 'red' }}>{ errors?.lastName }</span>
         <input type='submit'/>
-        <div class="names">
+        <div className="names">
           <ul>
           {names.map((name, index) => <li key={index}>{name.firstName}, {name.lastName}</li>)}
           </ul>
@@ -31,6 +46,19 @@ function App() {
       </form>
     </div>
   )
+}
+
+function buildErrors(formState) {
+  let errors = {};
+  const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+  Object.entries(formState).forEach(([key, value]) => {
+    if (!value) {
+      errors = { ...errors, [key]: `${nameDisplayMap[key]} is required` };
+    } else if (key === 'lastName' && !value.match(emailRegEx)) {
+      errors = { ...errors, [key]: `${nameDisplayMap[key]} format is invalid` };
+    }
+  });
+  return errors;
 }
 
 export default App;
